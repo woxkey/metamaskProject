856a97658b66d216af713e6c5e52f82f815cf1e9
Alen Bolatov <bolatovalen@gmail.com>
2023-04-03 15:29:23 +0600

    some fix

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -4,52 +4,61 @@ import 'react-circular-progressbar/dist/styles.css';
 import SvgLines from 'react-mt-svg-lines';
 import button from '../images/button.svg';
 import smallBtn from '../images/smallDot.svg';
+import fullBlackMatter from '../images/fullBlackMatter.svg';
+import layer1 from '../images/layer1.svg';
 
 const Explore: React.FunctionComponent = (): React.ReactElement => {
 	return (
 		<>
 			<div className="container mx-auto pt-[202px] flex justify-between">
 				<div className="relative pb-[45px] inline-block ">
-					<h1 className="text-[120px] leading-[120px] bg-transparent  relative  z-10">
+					<h1 className="text-[120px] leading-[120px] bg-transparent  relative  z-30">
 						Explore Your own planet
 					</h1>
-					<h2 className="text-[120px] leading-[119px] bg-opacity-0 z-10">
+					<h2 className="text-[120px] leading-[119px] bg-opacity-0 relative bg-transparent z-30">
 						In{' '}
-						<span className="text-[#171719]  myTextStroke bg-transparent  z-10">
+						<span className="text-[#171719]  myTextStroke bg-transparent  z-30">
 							our New
 						</span>{' '}
 						metaverse
 					</h2>
 
 					<img
-						className=" absolute top-[-50px] left-[664px] bg-transparent mix-blend-exclusion z-20"
+						className=" absolute top-[-50px] left-[664px] bg-transparent mix-blend-exclusion z-40"
 						src={Planet}
 						alt="#"
 					/>
-					<SvgLines animate={500} duration={10000} playback={'infinite'}>
-						<div className="caret-transparent">
-							<svg width={'120%'} className="lineSvg" viewBox="0 0 100 100">
-								<path
-									d=" M 50 30 A 20 20 0 1 1 50 70 A 20 20 0 1 1 50 30 Z"
-									stroke="#E75626"
-									strokeWidth="0.1"
-									fill="none"
-								/>
-							</svg>
-						</div>
-					</SvgLines>
-					<div className="  flex justify-center items-center absolute top-[-154px] right-[156px] py-[6px] px-3 bg-white rounded-[15px]">
+					<div className="relative z-20 top-[-235px] left-0">
+						<SvgLines animate={500} duration={10000} playback={'infinite'}>
+							<div className="caret-transparent ">
+								<svg width={'120%'} className="lineSvg" viewBox="0 0 100 100">
+									<path
+										d=" M 50 30 A 20 20 0 1 1 50 70 A 20 20 0 1 1 50 30 Z"
+										stroke="#E75626"
+										strokeWidth="0.1"
+										fill="none"
+									/>
+								</svg>
+							</div>
+						</SvgLines>
+					</div>
+					<div className=" z-20 flex justify-center items-center absolute top-[-151px] right-[144px] py-[6px] px-3 bg-white rounded-[15px]">
 						<span className="bg-transparent text-[#171719] ">Q1 2022</span>
 						<img className="bg-transparent h-6" src={button} alt="#" />
 					</div>
 					<img
-						className="absolute top-[100px] right-[-62px]"
+						className="absolute top-[100px] right-[-62px] z-20"
 						src={smallBtn}
 						alt="#"
 					/>
 					<img
-						className="absolute top-[343px] right-[170px]"
+						className="absolute top-[343px] right-[170px] z-20"
 						src={smallBtn}
+						alt="#"
+					/>
+					<img
+						className="w-[532px] h-[532px] absolute top-[-158px] right-[-85px] blackMatter"
+						src={fullBlackMatter}
 						alt="#"
 					/>
 				</div>
